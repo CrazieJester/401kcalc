847caf5ef9d42c3a9b4413ee51f71c7acd2c2923
Enzo <crazie321@gmail.com>
2021-03-25 16:34:50 -0500

    Updates

---
+++ precache-manifest.f8d787c86079c0e834b011e7cf8820c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028ae8264f266960a51ea5b0079cb83c",
+    "url": "/401kcalc/index.html"
+  },
+  {
+    "revision": "1ad3ab10ffeb2d2297e7",
+    "url": "/401kcalc/static/css/main.af2dd7d1.chunk.css"
+  },
+  {
+    "revision": "0c2f059f4c30c88e194e",
+    "url": "/401kcalc/static/js/2.c4600558.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/401kcalc/static/js/2.c4600558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad3ab10ffeb2d2297e7",
+    "url": "/401kcalc/static/js/main.b48df0dc.chunk.js"
+  },
+  {
+    "revision": "344e810171467b2ddbe5",
+    "url": "/401kcalc/static/js/runtime-main.56a9664a.js"
+  },
+  {
+    "revision": "fd589fe0d0b949036c6685e5d8e014ae",
+    "url": "/401kcalc/static/media/logo.fd589fe0.svg"
+  }
+]);
